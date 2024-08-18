b25c35794f48a4eb8af1db7421084134686e56ba
Durgeshkeshri <84857957+Durgeshkeshri@users.noreply.github.com>
2024-08-18 19:01:58 +0530

    firebase

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -43,3 +43,24 @@ export const Body = () => {
         </>
     )
 }
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDRUkDFe5YLU9ChJJ-p4XDZesMiNPzpPck",
+//   authDomain: "codersclub-7206d.firebaseapp.com",
+//   projectId: "codersclub-7206d",
+//   storageBucket: "codersclub-7206d.appspot.com",
+//   messagingSenderId: "913197893409",
+//   appId: "1:913197893409:web:5c93025b03a2544338a060",
+//   measurementId: "G-BK0RT4SM5D"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
